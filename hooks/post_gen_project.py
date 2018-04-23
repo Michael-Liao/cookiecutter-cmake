2975fcb1438c8e19d03aa1613322fb494eb4a9bf
Michael Liao <michael.liao1112@gmail.com>
2018-04-23 17:52:03 +0800

    adding hook (broken)

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 import os
-# import shutil # deleting dirs
+# import shutil
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 # def remove_dir(dirpath):
-#     shutil.rmtree(os.path.join(PROJECT_DIRECTORY, dirpath))
+    # shutil.rmtree(os.path.join(PROJECT_DIRECTORY, dirpath))
 
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
+
 if __name__ == '__main__':
-    if '{{cookiecutter.use_catch}}' == 'n':
+    if '{{ cookiecutter.use_catch }}' != 'y':
         pass
-        # remove_file('tests/catch.hpp')
-    if 'no' in '{{cookiecutter.command_line_interface}}':
+        # remove_file('vendor/catch/catch.hpp')
+    if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
         pass
         # remove_file('include/{{}}/clara.hpp')
 
